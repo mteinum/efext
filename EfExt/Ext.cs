75c87d84248a8a2d388522e1bcffca4f2f9f39fd
Morten Teinum <morten.teinum@gmail.com>
2013-04-25 00:47:41 +0200

    DRY

--- EfExt/Ext.cs
+++ EfExt/Ext.cs
@@ -2,56 +2,70 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace EfExt
 {
     public static class Ext
     {
+        private static readonly MethodInfo CompareTo = typeof(string).GetMethod("CompareTo", new[] { typeof(string) });
+        private static readonly Expression Zero = Expression.Constant(0);
+
+        private static IQueryable<TSource> StringCompare<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, string>> keySelector,
+            string value,
+            Func<Expression, Expression, Expression> method)
+        {
+            var exp = method(Expression.Call(
+                keySelector.Body, CompareTo, Expression.Constant(value)), Zero);
+
+            var lambda = Expression.Lambda<Func<TSource, bool>>(
+                exp, keySelector.Parameters[0]);
+
+            return source.Where(lambda);
+        }
+
+        public static IQueryable<TSource> GreaterThan<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, string>> keySelector,
+            string value)
+        {
+            return source.StringCompare(
+                keySelector,
+                value,
+                Expression.GreaterThanOrEqual);
+        }
+
+        public static IQueryable<TSource> LessThan<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, string>> keySelector,
+            string value)
+        {
+            return source.StringCompare(
+                keySelector,
+                value,
+                Expression.LessThanOrEqual);
+        }
+
         public static IQueryable<TSource> Between<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> keySelector,
             string lower, string upper)
         {
-            var zero = Expression.Constant(0);
-            var compareTo = typeof(string).GetMethod("CompareTo", new[] { typeof(string) });
-
-            var loExp = Expression.GreaterThanOrEqual(
-                Expression.Call(keySelector.Body, compareTo, Expression.Constant(lower)), zero);
-
-            var hiExp = Expression.LessThanOrEqual(
-                Expression.Call(keySelector.Body, compareTo, Expression.Constant(upper)), zero);
-
-            var lambda = Expression.Lambda<Func<TSource, bool>>(
-                Expression.AndAlso(loExp, hiExp), keySelector.Parameters[0]);
-
-            return source.Where(lambda);
+            return source.GreaterThan(keySelector, lower).LessThan(keySelector, upper);
         }
 
         // source.Where(lower.CompareTo(value) <= 0 && upper.CompareTo(value) >= 0);
+
         public static IQueryable<TSource> Between<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> lowerKeySelector,
             Expression<Func<TSource, string>> upperKeySelector,
             string value)
         {
-            var zero = Expression.Constant(0);
-            var item = new Expression[] { Expression.Constant(value) };
-            var compareTo = typeof(string).GetMethod("CompareTo", new[] { typeof(string) });
-
-            var lower = Expression.LessThanOrEqual(
-                Expression.Call(lowerKeySelector.Body, compareTo, item), zero);
-
-            var upper = Expression.GreaterThanOrEqual(
-                Expression.Call(upperKeySelector.Body, compareTo, item), zero);
-
-            var lambdaLower = Expression.Lambda<Func<TSource, bool>>(
-                lower,
-                lowerKeySelector.Parameters[0]);
-
-            var lambdaUpper = Expression.Lambda<Func<TSource, bool>>(
-                upper, upperKeySelector.Parameters[0]);
-
-            return source.Where(lambdaLower).Where(lambdaUpper);
+            return source.LessThan(lowerKeySelector, value)
+                         .GreaterThan(upperKeySelector, value);
         }
     }
 }
