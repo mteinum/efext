2eaaaebd6e86f7a7a96d9bb218c8226662e9004f
Morten Teinum <morten.teinum@gmail.com>
2013-04-25 00:56:41 +0200

    added overloads

--- EfExt/Ext.cs
+++ EfExt/Ext.cs
@@ -2,23 +2,25 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 
 namespace EfExt
 {
     public static class Ext
     {
-        private static readonly MethodInfo CompareTo = typeof(string).GetMethod("CompareTo", new[] { typeof(string) });
-        private static readonly Expression Zero = Expression.Constant(0);
-
+        /// <summary>
+        /// 
+        /// </summary>
         private static IQueryable<TSource> StringCompare<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> keySelector,
             string value,
             Func<Expression, Expression, Expression> method)
         {
+            var compareTo = typeof(string).GetMethod("CompareTo", new[] { typeof(string) });
+            var zero = Expression.Constant(0);
+
             var exp = method(Expression.Call(
-                keySelector.Body, CompareTo, Expression.Constant(value)), Zero);
+                keySelector.Body, compareTo, Expression.Constant(value)), zero);
 
             var lambda = Expression.Lambda<Func<TSource, bool>>(
                 exp, keySelector.Parameters[0]);
@@ -26,7 +28,10 @@ namespace EfExt
             return source.Where(lambda);
         }
 
-        public static IQueryable<TSource> GreaterThan<TSource>(
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IQueryable<TSource> GreaterThanOrEqual<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> keySelector,
             string value)
@@ -37,7 +42,24 @@ namespace EfExt
                 Expression.GreaterThanOrEqual);
         }
 
-        public static IQueryable<TSource> LessThan<TSource>(
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IQueryable<TSource> GreaterThan<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, string>> keySelector,
+            string value)
+        {
+            return source.StringCompare(
+                keySelector,
+                value,
+                Expression.GreaterThan);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IQueryable<TSource> LessThanOrEqual<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> keySelector,
             string value)
@@ -48,24 +70,69 @@ namespace EfExt
                 Expression.LessThanOrEqual);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IQueryable<TSource> LessThan<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, string>> keySelector,
+            string value)
+        {
+            return source.StringCompare(
+                keySelector,
+                value,
+                Expression.LessThan);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static IQueryable<TSource> Between<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> keySelector,
             string lower, string upper)
         {
-            return source.GreaterThan(keySelector, lower).LessThan(keySelector, upper);
+            return source.GreaterThanOrEqual(keySelector, lower)
+                         .LessThanOrEqual(keySelector, upper);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IQueryable<TSource> Between<TSource>(
+            this IQueryable<TSource> source,
+            string lower,
+            Expression<Func<TSource, string>> keySelector,
+            string upper)
+        {
+            return source.Between(keySelector, lower, upper);
         }
 
         // source.Where(lower.CompareTo(value) <= 0 && upper.CompareTo(value) >= 0);
 
+        /// <summary>
+        /// 
+        /// </summary>
         public static IQueryable<TSource> Between<TSource>(
             this IQueryable<TSource> source,
             Expression<Func<TSource, string>> lowerKeySelector,
             Expression<Func<TSource, string>> upperKeySelector,
             string value)
         {
-            return source.LessThan(lowerKeySelector, value)
-                         .GreaterThan(upperKeySelector, value);
+            return source.LessThanOrEqual(lowerKeySelector, value)
+                         .GreaterThanOrEqual(upperKeySelector, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IQueryable<TSource> Between<TSource>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, string>> lowerKeySelector,
+            string value,
+            Expression<Func<TSource, string>> upperKeySelector)
+        {
+            return source.Between(lowerKeySelector, upperKeySelector, value);
         }
     }
 }
