aa22a69957b862bb78a64948a5e46e9b0af91b11
Morten Teinum <morten.teinum@gmail.com>
2013-04-27 22:18:58 +0200

    Added example on how to sort a tree structure

--- EfExt.Tests/RecursiveTreeExtFixture.cs
+++ EfExt.Tests/RecursiveTreeExtFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -9,14 +10,16 @@ namespace EfExt.Tests
     {
         class Node
         {
-            public Node(int id) {
+            public Node(int id)
+            {
                 Id = id;
             }
 
             public int Id { get; private set; }
             public readonly List<Node> Children = new List<Node>();
 
-            public Node Add(Node node) {
+            public Node Add(Node node)
+            {
                 Children.Add(node);
                 return this;
             }
@@ -53,6 +56,52 @@ namespace EfExt.Tests
                                   .Where(n => n.Children.Empty());
 
             Assert.AreEqual(4, noChildren.Count());
+        }
+
+
+        [Test]
+        public void WithLookupFunction()
+        {
+            var items = new[]
+                {
+                    new Item(2, 1),
+                    new Item(3, 1),
+                    new Item(4, 2),
+                    new Item(1, 1),
+                    new Item(5, 1),
+                    new Item(6, 4)
+                };
+
+            var root = items.Single(x => x.Id == 1);
+
+            var sortedByTree = root.Recursive(items.ChildSelector).ToList();
+
+            var expected = new[] { 1, 2, 4, 6, 3, 5 };
+
+            Assert.IsTrue(expected.SequenceEqual(sortedByTree.Select(i => i.Id)));
+        }
+    }
+
+    public class Item
+    {
+        public int Id;
+        public int Parent;
+
+        public Item() { }
+        public Item(int id, int parent)
+        {
+            Id = id;
+            Parent = parent;
+        }
+    }
+
+    public static class ItemExt
+    {
+        public static IEnumerable<Item> ChildSelector(
+            this IEnumerable<Item> source,
+            Item item)
+        {
+            return source.Where(i => i.Parent == item.Id && i.Parent != i.Id);
         }
     }
 }
--- README.md
+++ README.md
@@ -101,6 +101,55 @@ selector is used for finding the children, that must be of the same type as node
         }
 ```
 
+How to sort a id/parent object by it's hierarchy
+
+```c#
+[Test]
+public void WithLookupFunction()
+{
+	var items = new[]
+	{
+		new Item(2, 1),
+		new Item(3, 1),
+		new Item(4, 2),
+		new Item(1, 1),
+		new Item(5, 1),
+		new Item(6, 4)
+	};
+
+	var root = items.Single(x => x.Id == 1);
+
+	var sortedByTree = root.Recursive(items.ChildSelector).ToList();
+
+	var expected = new[] { 1, 2, 4, 6, 3, 5 };
+
+	Assert.IsTrue(expected.SequenceEqual(sortedByTree.Select(i => i.Id)));
+}
+
+public class Item
+{
+	public int Id;
+	public int Parent;
+
+	public Item() { }
+	public Item(int id, int parent)
+	{
+		Id = id;
+		Parent = parent;
+	}
+}
+
+public static class ItemExt
+{
+	public static IEnumerable<Item> ChildSelector(
+		this IEnumerable<Item> source,
+		Item item)
+	{
+		return source.Where(i => i.Parent == item.Id && i.Parent != i.Id);
+	}
+}
+```
+
 See the testproject for more examples.
 
 https://twitter.com/mteinum
