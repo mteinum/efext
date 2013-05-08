7e8803303deb861bb6f2caa01a9296f4bcc35648
Morten Teinum <morten.teinum@gmail.com>
2013-05-08 22:02:56 +0200

    Added Range class

---
+++ EfExt.Tests/RangeFixture.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace EfExt.Tests
+{
+    [TestFixture]
+    public class RangeFixture
+    {
+        [Test]
+        public void GetRangesSingle()
+        {
+            var ranges = Range.Parse("80085364").ToList();
+
+            Assert.AreEqual(1, ranges.Count);
+            Assert.AreEqual("80085364", ranges[0].From);
+            Assert.AreEqual("80085364", ranges[0].To);
+        }
+
+        [Test]
+        public void GetRangesTwoSingle()
+        {
+            var ranges = Range.Parse("80085364,80085365").ToList();
+
+            Assert.AreEqual(2, ranges.Count);
+            Assert.AreEqual("80085364", ranges[0].From);
+            Assert.AreEqual("80085364", ranges[0].To);
+
+            Assert.AreEqual("80085365", ranges[1].From);
+            Assert.AreEqual("80085365", ranges[1].To);
+        }
+
+        [Test]
+        public void GetRangesSingleRange()
+        {
+            var ranges = Range.Parse("80085364,80085365-80085366").ToList();
+
+            Assert.AreEqual(2, ranges.Count);
+            Assert.AreEqual("80085364", ranges[0].From);
+            Assert.AreEqual("80085364", ranges[0].To);
+
+            Assert.AreEqual("80085365", ranges[1].From);
+            Assert.AreEqual("80085366", ranges[1].To);
+        }
+
+        [Test]
+        public void Compress0()
+        {
+            var r = new Range { From = "40401000", To = "40401000" };
+            Assert.AreEqual("40401000", r.Compress());
+            Assert.AreEqual(1, r.Numbers.Count());
+            Assert.AreEqual("40401000", r.Numbers.First());
+        }
+
+        [Test]
+        public void Compress10()
+        {
+            var r = new Range { From = "40401000", To = "40401009" };
+            Assert.AreEqual("40401000-9", r.Compress());
+        }
+
+        [Test]
+        public void Compress50()
+        {
+            var r = new Range { From = "40401000", To = "40401049" };
+            Assert.AreEqual("40401000-49", r.Compress());
+        }
+
+        [Test]
+        public void Compress1000()
+        {
+            var r = new Range { From = "40401000", To = "40401999" };
+            Assert.AreEqual("40401000-999", r.Compress());
+        }
+
+        [Test]
+        public void CompressDifferentRange()
+        {
+            var r = new Range { From = "40000000", To = "50000000" };
+            Assert.AreEqual("40000000-50000000", r.Compress());
+        }
+
+        [Test]
+        public void ParseCompressed100()
+        {
+            var r = Range.Parse("40401000-99").First();
+
+            Assert.AreEqual("40401000", r.From);
+            Assert.AreEqual("40401099", r.To);
+
+            Assert.AreEqual(100, r.Numbers.Count());
+            Assert.AreEqual("40401000", r.Numbers.First());
+            Assert.AreEqual("40401099", r.Numbers.Last());
+        }
+    }
+}
---
+++ EfExt/Range.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace EfExt
+{
+    public sealed class Range
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public IEnumerable<string> Numbers
+        {
+            get
+            {
+                long start = Int64.Parse(From);
+                long to = Int64.Parse(To);
+
+                return Enumerable.Range(0, (int)(to - start) + 1)
+                                 .Select(i => start + i)
+                                 .Select(l => l.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+
+        public static IEnumerable<Range> Parse(string line)
+        {
+            return line.Split(',').Select(ParseRange);
+        }
+
+        private static Range ParseRange(string item)
+        {
+            var arr = item.Split('-');
+
+            return new Range
+            {
+                From = arr[0],
+                To = arr.Length == 2 ? Expand(arr[0], arr[1]) : arr[0]
+            };
+        }
+
+        private static string Expand(string from, string to)
+        {
+            var numberOfCharToPrepend = from.Length - to.Length;
+
+            if (numberOfCharToPrepend == 0)
+                return to;
+
+            return String.Format("{0}{1}", from.Substring(0, numberOfCharToPrepend), to);
+        }
+
+        public override string ToString()
+        {
+            return Compress();
+        }
+
+        public string Compress()
+        {
+            var start = FirstDifference();
+
+            if (start == From.Length)
+                return From;
+
+            return String.Format("{0}-{1}", From, To.Substring(start));
+        }
+
+        private int FirstDifference()
+        {
+            int i;
+
+            for (i = 0; i < From.Length; i++)
+            {
+                if (From[i] != To[i])
+                    break;
+            }
+
+            return i;
+        }
+    }
+}
